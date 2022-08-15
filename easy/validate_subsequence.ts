0d9ce4aade9b2af2bf5e88779b81215be8d99688
Philippe Fanaro <philippefanaro@MacBook-Air-de-Philippe.local>
2022-08-15 10:40:36 -0300

    initial attempt at problem 1

---
+++ easy/validate_subsequence.ts
@@ -0,0 +1,28 @@
+const isValidSubsequence = (array: number[], sequence: number[]): boolean => {
+  let sliceableArray: number[] = array;
+  const checkedSeqNumbers: boolean[] = [];
+
+  for (const seqNum of sequence) {
+    const indSeqNum: number = sliceableArray.findIndex(
+      (n: number) => n === seqNum
+    );
+
+    if (typeof indSeqNum === "number") {
+      sliceableArray = sliceableArray.slice(indSeqNum, sliceableArray.length);
+
+    console.log(sliceableArray)
+      checkedSeqNumbers.push(true);
+      if (checkedSeqNumbers.length === sequence.length) return true;
+    } else {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+const array1: number[] = [5, 1, 22, 25, 6, -1, 8, 10];
+const sequence1: number[] = [1, 6, 10, -1];
+
+console.log(isValidSubsequence(array1, sequence1))
+
