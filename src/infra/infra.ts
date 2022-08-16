4b2a5be68781bb8c8b2c917ee5d4e7cb30a80606
Philippe Fanaro <philippefanaro@gmail.com>
2022-08-15 23:28:02 -0300

    boilerplate for the river sizes problem

--- src/infra/infra.ts
+++ src/infra/infra.ts
@@ -1,9 +1,17 @@
 type TestFunc = () => void;
 
 export const test = (description: string, testFunc: TestFunc): void => {
+  const dashNum: number = 40;
+
+  console.log("-".repeat(dashNum));
   console.log(description);
+  console.log("-".repeat(dashNum));
+  console.log("");
 
   testFunc();
+
+  console.log("");
+  console.log("-".repeat(dashNum));
 };
 
 export const expect = (left: any, right: any): void => {
