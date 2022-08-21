11bfc5e88e875bc23b87a13710db9eccb089da92
Philippe Fanaro <philippefanaro@gmail.com>
2022-08-21 15:50:35 -0300

    solved the Two Number Sum | better solutions for it, from Clem himself

--- src/easy/two_number_sum.test.ts
+++ src/easy/two_number_sum.test.ts
@@ -1,9 +1,30 @@
 import * as chai from "chai";
-import { twoNumberSum } from "./two_number_sum";
 
-it("Test Case #1", function () {
-  const output = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
-  chai.expect(output.length === 2).to.deep.equal(true);
-  chai.expect(output.includes(11)).to.deep.equal(true);
-  chai.expect(output.includes(-1)).to.deep.equal(true);
+import {
+  twoNumberSum,
+  twoNumberSumHash,
+  twoNumberSumPointers,
+} from "./two_number_sum";
+
+describe("Two Number Sum", () => {
+  it("Test Case #1 | Double Fors", () => {
+    const output: number[] = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+    chai.expect(output.length === 2).to.deep.equal(true);
+    chai.expect(output.includes(11)).to.deep.equal(true);
+    chai.expect(output.includes(-1)).to.deep.equal(true);
+  });
+
+  it("Test Case #1 | Hash Table", () => {
+    const output: number[] = twoNumberSumHash([3, 5, -4, 8, 11, 1, -1, 6], 10);
+    chai.expect(output.length === 2).to.deep.equal(true);
+    chai.expect(output.includes(11)).to.deep.equal(true);
+    chai.expect(output.includes(-1)).to.deep.equal(true);
+  });
+
+  it("Test Case #1 | Left and Right Pointers", () => {
+    const output: number[] = twoNumberSumPointers([3, 5, -4, 8, 11, 1, -1, 6], 10);
+    chai.expect(output.length === 2).to.deep.equal(true);
+    chai.expect(output.includes(11)).to.deep.equal(true);
+    chai.expect(output.includes(-1)).to.deep.equal(true);
+  });
 });
--- src/easy/two_number_sum.ts
+++ src/easy/two_number_sum.ts
@@ -1,5 +1,58 @@
+type Index = number;
+type Length = number;
+
+// 1
+// With a dual for-loop, we get C(n, 2) = n(n-1)/2 -> O(n^2)
+// Time: O(n^2) | Space: O(1)
 export const twoNumberSum = (array: number[], targetSum: number) => {
-  console.log(array);
-  console.log(targetSum);
-  return [-1, -1];
+  const length: Length = array.length;
+
+  for (let i: Index = 0; i < length; i++) {
+    for (let j: Index = i + 1; j < length; j++) {
+      if (array[i] + array[j] === targetSum) return [array[i], array[j]];
+    }
+  }
+
+  return [];
+};
+
+// 2
+// Most efficient version: Hash Table + Equation
+// Time: O(n) | Space: O(n) (registering on the hash table)
+export const twoNumberSumHash = (array: number[], targetSum: number) => {
+  const numsTable: Map<number, boolean> = new Map();
+
+  for (const num of array) {
+    const potentialMatch: number = targetSum - num;
+
+    if (numsTable.has(potentialMatch)) return [potentialMatch, num];
+
+    numsTable.set(num, true);
+  }
+
+  return [];
+};
+
+// 3
+// Pointer Method
+// Time: O(nlogn) (Quicksort-like sorting) | Space: O(1)
+export const twoNumberSumPointers = (array: number[], targetSum: number) => {
+  const sortedArray: number[] = array.sort((a: number, b: number) => a - b);
+  let leftPointer: Index = 0;
+  let rightPointer: Index = array.length - 1;
+  let currentSum: number;
+
+  while (leftPointer < rightPointer) {
+    currentSum = sortedArray[leftPointer] + sortedArray[rightPointer];
+
+    if (currentSum === targetSum) {
+      return [sortedArray[leftPointer], sortedArray[rightPointer]];
+    } else if (currentSum < targetSum) {
+      leftPointer += 1;
+    } else if (currentSum > targetSum) {
+      rightPointer -= 1;
+    }
+  }
+
+  return [];
 };
