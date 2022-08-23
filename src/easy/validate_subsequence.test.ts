56691824c60d69957c09a5c7ff23ad7ec3444fe8
Philippe Fanaro <philippefanaro@gmail.com>
2022-08-23 15:09:32 -0300

    Clem's solution to the Validate Subsequence problem

--- src/easy/validate_subsequence.test.ts
+++ src/easy/validate_subsequence.test.ts
@@ -1,6 +1,9 @@
 import * as chai from "chai";
 
-import { isValidSubsequence } from "./validate_subsequence";
+import {
+  isValidSubsequence,
+  isValidSubsequenceClem,
+} from "./validate_subsequence";
 
 describe("Validate Subsequence", () => {
   it("Test Case #1", () => {
@@ -14,5 +17,18 @@ describe("Validate Subsequence", () => {
     const array2 = [5, 1, 22, 25, 6, -1, 8, 10];
     const sequence2 = [1, 6, -1, 10];
     chai.expect(isValidSubsequence(array2, sequence2)).to.deep.equal(true);
+  });
+
+  it("Clem | Test Case #1", () => {
+    const array1: number[] = [5, 1, 22, 25, 6, -1, 8, 10];
+    const sequence1: number[] = [1, 6, -1];
+
+    chai.expect(isValidSubsequenceClem(array1, sequence1)).to.deep.equal(true);
+  });
+
+  it("Clem | Test Case #2", function () {
+    const array2 = [5, 1, 22, 25, 6, -1, 8, 10];
+    const sequence2 = [1, 6, -1, 10];
+    chai.expect(isValidSubsequenceClem(array2, sequence2)).to.deep.equal(true);
   });
 });
--- src/easy/validate_subsequence.ts
+++ src/easy/validate_subsequence.ts
@@ -1,30 +1,51 @@
 import { shallowExpect, customTest } from "../infra/infra";
 
+type Index = number;
+type Length = number;
+
+// Minor problem: my solution slices arrays,
+// which might imply copying inefficiencies.
 export const isValidSubsequence = (
   array: number[],
   sequence: number[]
 ): boolean => {
   let sliceableArray: number[] = [...array];
-  sliceableArray[0] = 10000;
+  const sequenceLength: Length = sequence.length;
 
   const checkedSeqNumbers: boolean[] = [];
 
   for (const seqNum of sequence) {
-    const indSeqNum: number = sliceableArray.findIndex(
+    const indSeqNum: Index = sliceableArray.findIndex(
       (n: number) => n === seqNum
     );
 
-    // findIndex gives back -1 if the number isn't in it.
+    // `.findIndex` gives back `-1` if the number isn't in it.
     if (indSeqNum === -1) return false;
 
     sliceableArray = sliceableArray.slice(indSeqNum + 1, sliceableArray.length);
 
     checkedSeqNumbers.push(true);
 
-    if (checkedSeqNumbers.length === sequence.length) return true;
+    if (checkedSeqNumbers.length === sequenceLength) return true;
   }
 
   return false;
+};
+
+// O(n) T | O(1)
+export const isValidSubsequenceClem = (
+  array: number[],
+  sequence: number[]
+): boolean => {
+  let seqIdx: Index = 0;
+  const sequenceLength: Length = sequence.length;
+
+  for (const arrayNum of array) {
+    if (seqIdx === sequenceLength) break;
+    if (sequence[seqIdx] === arrayNum) seqIdx++;
+  }
+
+  return seqIdx === sequenceLength;
 };
 
 const array1: number[] = [5, 1, 22, 25, 6, -1, 8, 10];
