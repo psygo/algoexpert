e50aa5e008e99804e1c40eff838afac31e7fd154
Philippe Fanaro <philippefanaro@gmail.com>
2022-08-27 12:13:22 -0300

    Clem's iterative solution to the find closest value in BST

--- src/easy/find_closest_value_in_bst.test.ts
+++ src/easy/find_closest_value_in_bst.test.ts
@@ -1,17 +1,38 @@
 import * as chai from "chai";
-import { BST, findClosestValueInBst } from "./find_closest_value_in_bst";
+import {
+  BST,
+  findClosestValueInBst,
+  findClosestValueInBstClem,
+} from "./find_closest_value_in_bst";
 
-it("Test Case #1", function () {
-  const root = new BST(10);
-  root.left = new BST(5);
-  root.left.left = new BST(2);
-  root.left.left.left = new BST(1);
-  root.left.right = new BST(5);
-  root.right = new BST(15);
-  root.right.left = new BST(13);
-  root.right.left.right = new BST(14);
-  root.right.right = new BST(22);
-  const expected = 13;
-  const actual = findClosestValueInBst(root, 12);
-  chai.expect(actual).to.deep.equal(expected);
+describe("Find Closest Value in BST", () => {
+  it("Test Case #1", function () {
+    const root = new BST(10);
+    root.left = new BST(5);
+    root.left.left = new BST(2);
+    root.left.left.left = new BST(1);
+    root.left.right = new BST(5);
+    root.right = new BST(15);
+    root.right.left = new BST(13);
+    root.right.left.right = new BST(14);
+    root.right.right = new BST(22);
+    const expected = 13;
+    const actual = findClosestValueInBst(root, 12);
+    chai.expect(actual).to.deep.equal(expected);
+  });
+
+  it("Clem | Test Case #1", function () {
+    const root = new BST(10);
+    root.left = new BST(5);
+    root.left.left = new BST(2);
+    root.left.left.left = new BST(1);
+    root.left.right = new BST(5);
+    root.right = new BST(15);
+    root.right.left = new BST(13);
+    root.right.left.right = new BST(14);
+    root.right.right = new BST(22);
+    const expected = 13;
+    const actual = findClosestValueInBstClem(root, 12);
+    chai.expect(actual).to.deep.equal(expected);
+  });
 });
--- src/easy/find_closest_value_in_bst.ts
+++ src/easy/find_closest_value_in_bst.ts
@@ -11,6 +11,8 @@ export class BST {
 }
 
 // There will always only be one value in the tree.
+
+// "Inefficient Explore Everything"
 // O(N) T | O(1) S
 export const findClosestValueInBst = (tree: BST, target: number): number => {
   let currentClosestValue: number = tree.value;
@@ -26,4 +28,25 @@ export const findClosestValueInBst = (tree: BST, target: number): number => {
   recursiveExplore(tree);
 
   return currentClosestValue;
+};
+
+// O(logn) T (if unbalanced: O(N)
+// O(logn) S (if recursion, otherwise O(1) if iteratively)
+export const findClosestValueInBstClem = (
+  tree: BST,
+  target: number
+): number => {
+  let closest: number = tree.value;
+  let currentNode: BST | null = tree;
+
+  while (currentNode) {
+    if (!currentNode) return closest;
+    else if (Math.abs(target - closest) > Math.abs(target - currentNode.value))
+      closest = currentNode.value;
+    else if (target < currentNode.value) currentNode = tree.left;
+    else if (target > currentNode.value) currentNode = tree.right;
+    else break;
+  }
+
+  return closest;
 };
