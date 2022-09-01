f56264d90cf2ae2ccffaa957d56d38ec146d4241
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-01 16:17:47 -0300

    solved the minimum waiting time problem

--- src/easy/minimum_waiting_time.test.ts
+++ src/easy/minimum_waiting_time.test.ts
@@ -4,6 +4,8 @@ import { minimumWaitingTime } from "./minimum_waiting_time";
 describe("Minimum Waiting Time", () => {
   it("Test Case #1", function () {
     const queries = [3, 2, 1, 2, 6];
+    // [1, 2, 2, 3]
+    // [0, 1, 1+2=3, 3+2=5, 5+3=8] -> 1+3+5+8 = 17
     const expected = 17;
     const actual = minimumWaitingTime(queries);
     chai.expect(actual).to.deep.equal(expected);
--- src/easy/minimum_waiting_time.ts
+++ src/easy/minimum_waiting_time.ts
@@ -1,4 +1,17 @@
-export const minimumWaitingTime = (queries: number[]): number => {
-  console.log(queries);
-  return -1;
+type Query = number;
+
+// O(nlogn) T | O(n) S
+export const minimumWaitingTime = (queries: Query[]): Query => {
+  const orderedQueries: Query[] = queries.sort((a: Query, b: Query) => a - b);
+  orderedQueries.pop();
+
+  // Probably could have done with a `.map` as well...
+  // Didn't need a second array for it, which would make it O(1)
+  const queriesQueue: Query[] = [0];
+  for (const q of orderedQueries) {
+    const lastOnQueue: Query = queriesQueue[queriesQueue.length - 1];
+    queriesQueue.push(q + lastOnQueue);
+  }
+
+  return queriesQueue.reduce((p: Query, c: Query) => p + c);
 };
