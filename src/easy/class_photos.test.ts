1cd0fafea78174b4f7c39ad82bfd9ca505e33c8c
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-02 23:23:11 -0300

    solved the class photos problem

--- src/easy/class_photos.test.ts
+++ src/easy/class_photos.test.ts
@@ -4,8 +4,26 @@ import { classPhotos } from "./class_photos";
 describe("Class Photos", () => {
   it("Test Case #1", function () {
     const redShirtHeights = [5, 8, 1, 3, 4];
+    // Ordered Red:  [1, 3, 4, 5, 8]
     const blueShirtHeights = [6, 9, 2, 4, 5];
+    // Ordered Blue: [2, 4, 5, 6, 9]
     const expected = true;
+    const actual = classPhotos(redShirtHeights, blueShirtHeights);
+    chai.expect(actual).to.deep.equal(expected);
+  });
+
+  it("Test Case #2", function () {
+    const redShirtHeights = [6];
+    const blueShirtHeights = [6];
+    const expected = false;
+    const actual = classPhotos(redShirtHeights, blueShirtHeights);
+    chai.expect(actual).to.deep.equal(expected);
+  });
+
+  it("Test Case #3", function () {
+    const redShirtHeights = [5, 4];
+    const blueShirtHeights = [4, 5];
+    const expected = false;
     const actual = classPhotos(redShirtHeights, blueShirtHeights);
     chai.expect(actual).to.deep.equal(expected);
   });
--- src/easy/class_photos.ts
+++ src/easy/class_photos.ts
@@ -1,8 +1,29 @@
+type Height = number;
+
+type Length = number;
+type Index = number;
+
 export const classPhotos = (
-  redShirtHeights: number[],
-  blueShirtHeights: number[]
+  redShirtHeights: Height[],
+  blueShirtHeights: Height[]
 ): boolean => {
-  console.log(redShirtHeights);
-  console.log(blueShirtHeights);
-  return false;
+  const orderedRed: Height[] = redShirtHeights.sort(
+    (a: Height, b: Height): Height => a - b
+  );
+  const orderedBlue: Height[] = blueShirtHeights.sort(
+    (a: Height, b: Height): Height => a - b
+  );
+  const length: Length = redShirtHeights.length;
+
+  const ahead: Height[] =
+    orderedRed[0] < orderedBlue[0] ? orderedRed : orderedBlue;
+  const back: Height[] =
+    orderedRed[0] > orderedBlue[0] ? orderedRed : orderedBlue;
+
+  const aheadWorks: boolean[] = [];
+  for (let i: Index = 0; i < length; i++) {
+    aheadWorks.push(ahead[i] < back[i] ? true : false);
+  }
+
+  return aheadWorks.reduce((p: boolean, c: boolean) => p && c);
 };
