a49577f5f3479cd7b86eaaec2ddf678134343316
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-04 15:46:22 -0300

    solved the Tandem Bicycle Problem

--- src/easy/tandem_bicycle.ts
+++ src/easy/tandem_bicycle.ts
@@ -1,7 +1,26 @@
+type Speed = number;
+
+type Length = number;
+type Index = number;
+
+// O(nlogn) T | O(1) S (if able to sort in-place)
 export const tandemBicycle = (
-  redShirtSpeeds: number[],
-  blueShirtSpeeds: number[],
+  redShirtSpeeds: Speed[],
+  blueShirtSpeeds: Speed[],
   fastest: boolean
-): number => {
-  return -1;
+): Speed => {
+  const numOfRiders: Length = redShirtSpeeds.length;
+  redShirtSpeeds.sort((a: Speed, b: Speed) => a - b);
+  blueShirtSpeeds.sort((a: Speed, b: Speed) => a - b);
+
+  let sumSpeed: Speed = 0;
+  for (let i: Index = 0; i < numOfRiders; i++)
+    fastest
+      ? (sumSpeed += Math.max(
+          redShirtSpeeds[i],
+          blueShirtSpeeds[numOfRiders - i - 1]
+        ))
+      : (sumSpeed += Math.max(redShirtSpeeds[i], blueShirtSpeeds[i]));
+
+  return sumSpeed;
 };
