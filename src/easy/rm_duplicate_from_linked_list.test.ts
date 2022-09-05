0339ea7d79c8a1ccec4b2fc76f0e90534d1e0fce
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-05 11:43:27 -0300

    solved the rm duplicate from linked list problem

--- src/easy/rm_duplicate_from_linked_list.test.ts
+++ src/easy/rm_duplicate_from_linked_list.test.ts
@@ -3,11 +3,33 @@ import LinkedList, {
   removeDuplicatesFromLinkedList,
 } from "./rm_duplicate_from_linked_list";
 
-it("Test Case #1", function () {
-  const input = addMany(new LinkedList(1), [1, 3, 4, 4, 4, 5, 6, 6]);
-  const expected = addMany(new LinkedList(1), [3, 4, 5, 6]);
-  const actual = removeDuplicatesFromLinkedList(input);
-  chai.expect(getNodesInArray(actual)).to.deep.equal(getNodesInArray(expected));
+describe("Remove Duplicate from Linked List", () => {
+  it("Test Case #1", function () {
+    const input = addMany(new LinkedList(1), [1, 3, 4, 4, 4, 5, 6, 6]);
+    const expected = addMany(new LinkedList(1), [3, 4, 5, 6]);
+    const actual = removeDuplicatesFromLinkedList(input);
+    chai
+      .expect(getNodesInArray(actual))
+      .to.deep.equal(getNodesInArray(expected));
+  });
+
+  it("Test Case #2", function () {
+    const input = new LinkedList(1);
+    const expected = new LinkedList(1);
+    const actual = removeDuplicatesFromLinkedList(input);
+    chai
+      .expect(getNodesInArray(actual))
+      .to.deep.equal(getNodesInArray(expected));
+  });
+
+  it("Test Case #3", function () {
+    const input = addMany(new LinkedList(1), [1]);
+    const expected = new LinkedList(1);
+    const actual = removeDuplicatesFromLinkedList(input);
+    chai
+      .expect(getNodesInArray(actual))
+      .to.deep.equal(getNodesInArray(expected));
+  });
 });
 
 function addMany(linkedList: LinkedList, values: number[]) {
--- src/easy/rm_duplicate_from_linked_list.ts
+++ src/easy/rm_duplicate_from_linked_list.ts
@@ -8,8 +8,30 @@ export default class LinkedList {
   }
 }
 
+// O(n) T | O(1) S
 export const removeDuplicatesFromLinkedList = (
   linkedList: LinkedList
 ): LinkedList => {
+  let current = linkedList;
+  let next = linkedList.next;
+  while (true) {
+    if (next === null) break;
+    else {
+      if (current.value === next.value) {
+        if (next.next === null) {
+          current.next = null;
+          next = null;
+        } else {
+          current.next = next.next;
+          next = current.next;  
+        }
+      }
+      else {
+        current = next;
+        next = current.next;
+      }
+    }
+  }
+
   return linkedList;
 };
