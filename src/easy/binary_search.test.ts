107a3cdb8bbf60351c76fe80143eadd511267485
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-06 21:03:08 -0300

    solved the binary search problem

--- src/easy/binary_search.test.ts
+++ src/easy/binary_search.test.ts
@@ -1,8 +1,32 @@
 import * as chai from "chai";
 import { binarySearch } from "./binary_search";
 
-it("Test Case #1", function () {
-  chai
-    .expect(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33))
-    .to.deep.equal(3);
+describe("Binary Search", () => {
+  it("Test Case #1", function () {
+    chai
+      .expect(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33))
+      .to.deep.equal(3);
+  });
+
+  it("Test Case #2", function () {
+    chai.expect(binarySearch([], 1)).to.deep.equal(-1);
+  });
+
+  it("Test Case #3", function () {
+    chai.expect(binarySearch([1], 1)).to.deep.equal(0);
+  });
+
+  it("Test Case #4", function () {
+    chai.expect(binarySearch([0, 0, 1, 3], 1)).to.deep.equal(2);
+  });
+
+  it("Test Case #5", function () {
+    chai.expect(binarySearch([1, 5, 23, 111], 111)).to.deep.equal(3);
+  });
+
+  it("Test Case #6", function () {
+    chai
+      .expect(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 73))
+      .to.deep.equal(9);
+  });
 });
--- src/easy/binary_search.ts
+++ src/easy/binary_search.ts
@@ -1,3 +1,40 @@
+type Index = number;
+type Length = number;
+
+// O(logn) T | O(1) S
 export const binarySearch = (array: number[], target: number): number => {
-  return -1;
+  let currentLength: Length = array.length;
+
+  const lowerBound = (): Index => {
+    const lower: Index = Math.floor(currentLength / 2) - 1;
+    return lower >= 0 ? lower : 0;
+  };
+  const upperBound = (): Index => {
+    const upper: Index = Math.floor(currentLength / 2);
+    const ceiledUpper = upper <= currentLength - 1 ? upper : currentLength - 1;
+    return ceiledUpper >= 0 ? ceiledUpper : 0;
+  };
+
+  let leftPointer: Index = 0;
+  let leftEndPointer: Index = lowerBound();
+  let rightPointer: Index = upperBound();
+  let rightEndPointer: Index = currentLength - 1;
+
+  while (true) {
+    if (leftPointer === rightEndPointer && array[leftPointer] !== target)
+      return -1;
+    else if (target === array[leftPointer]) return leftPointer;
+    else if (target === array[rightEndPointer]) return rightEndPointer;
+    else if (target > array[leftEndPointer]) {
+      currentLength = rightEndPointer - rightPointer;
+      leftPointer = rightPointer;
+      leftEndPointer = rightPointer + lowerBound();
+      rightPointer = rightPointer + upperBound();
+    } else {
+      currentLength = leftEndPointer - leftPointer;
+      rightEndPointer = leftEndPointer;
+      leftEndPointer = lowerBound();
+      rightPointer = upperBound();
+    }
+  }
 };
