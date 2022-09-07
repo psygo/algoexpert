3c8a0d270bab86f786ffa417b7ec672dda566b1a
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-07 16:13:50 -0300

    solved the find three largest numbers

--- src/easy/find_three_largest_numbers.test.ts
+++ src/easy/find_three_largest_numbers.test.ts
@@ -1,10 +1,20 @@
 import * as chai from "chai";
 import { findThreeLargestNumbers } from "./find_three_largest_numbers";
 
-it("Test Case #1", function () {
-  chai
-    .expect(
-      findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7])
-    )
-    .to.deep.equal([18, 141, 541]);
+describe("Find Three Largest Numbers", () => {
+  it("Test Case #1", function () {
+    chai
+      .expect(
+        findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7])
+      )
+      .to.deep.equal([18, 141, 541]);
+  });
+
+  it("Test Case #2", function () {
+    chai
+      .expect(
+        findThreeLargestNumbers([55, 43, 11, 3, -3, 10])
+      )
+      .to.deep.equal([11, 43, 55]);
+  });
 });
--- src/easy/find_three_largest_numbers.ts
+++ src/easy/find_three_largest_numbers.ts
@@ -1,3 +1,32 @@
+type Index = number;
+type Length = number;
+
+const minusInf: number = -100000;
+
+// O(n) T | O(1) S
+// It's kind of a fixed stack implementation
 export const findThreeLargestNumbers = (array: number[]): number[] => {
-  return [];
+  const largestNums: [number, number, number] = [
+    minusInf,
+    minusInf + 1,
+    minusInf + 2,
+  ];
+  const length: Length = array.length;
+
+  for (let i: Index = 0; i < length; i++) {
+    const current: number = array[i];
+
+    if (current > largestNums[2]) {
+      largestNums[0] = largestNums[1];
+      largestNums[1] = largestNums[2];
+      largestNums[2] = current;
+    } else if (current > largestNums[1]) {
+      largestNums[0] = largestNums[1];
+      largestNums[1] = current;
+    } else if (current > largestNums[0]) {
+      largestNums[0] = current;
+    }
+  }
+
+  return largestNums;
 };
