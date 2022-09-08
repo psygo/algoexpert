33f3dad008456d65441a91cea3be3622aa2f56ef
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 15:14:07 -0300

    solved the palindrome check problem

--- src/easy/palindrome_check.ts
+++ src/easy/palindrome_check.ts
@@ -1,3 +1,13 @@
+type Length = number;
+type Index = number;
+
 export const isPalindrome = (string: string): boolean => {
-  return false;
+  const length: Length = string.length;
+  const afterMidPoint: Index = Math.floor(length) - 1;
+
+  let isPal: boolean = true;
+  for (let i: Index = 0; i < afterMidPoint; i++)
+    if (string[i] !== string[length - i - 1]) isPal = false;
+
+  return isPal;
 };
