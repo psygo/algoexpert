2f8ffc89978f310b3fd2cf839a3fe02968425de6
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 15:16:53 -0300

    minor improvements to palindrome check

--- src/easy/palindrome_check.ts
+++ src/easy/palindrome_check.ts
@@ -1,12 +1,13 @@
 type Length = number;
 type Index = number;
 
+// O(n) T | O(1) S
 export const isPalindrome = (string: string): boolean => {
   const length: Length = string.length;
-  const afterMidPoint: Index = Math.floor(length) - 1;
+  const beforeMidPoint: Index = Math.floor(length) - 1;
 
   let isPal: boolean = true;
-  for (let i: Index = 0; i < afterMidPoint; i++)
+  for (let i: Index = 0; i < beforeMidPoint; i++)
     if (string[i] !== string[length - i - 1]) isPal = false;
 
   return isPal;
