5dcd55008b5a44d1fdf903d329ef1e16d1ac6af9
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 14:07:34 -0300

    solved bubble sort

--- src/easy/bubble_sort.ts
+++ src/easy/bubble_sort.ts
@@ -1,3 +1,19 @@
+type Index = number;
+type Length = number;
+
+// O(n^2) T | O(1) S
 export const bubbleSort = (array: number[]): number[] => {
+  const length: Length = array.length;
+
+  for (let j: Index = 0; j < length; j++) {
+    for (let i: Index = 0; i < length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        const temp: number = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+
   return array;
 };
