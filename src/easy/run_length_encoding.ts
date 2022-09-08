251903a4a29e1186480b012ea991a3ee909482cf
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 17:11:41 -0300

    minor comment to run length encoding

--- src/easy/run_length_encoding.ts
+++ src/easy/run_length_encoding.ts
@@ -1,6 +1,7 @@
 type Index = number;
 type Length = number;
 
+// O(n) T | O(n) S
 export const runLengthEncoding = (string: string): string => {
   const length: Length = string.length;
   const encoded: string[] = [];
@@ -9,14 +10,6 @@ export const runLengthEncoding = (string: string): string => {
   let cacheCounter: number = 1;
   for (let i: Index = 1; i < length; i++) {
     const nextLetter: string = string[i];
-
-    console.log("--------------------------------------");
-    console.log(string[i]);
-    console.log(encoded);
-    console.log(cache);
-    console.log(cacheCounter);
-    console.log(cache !== nextLetter);
-    console.log("--------------------------------------");
 
     if (cache !== nextLetter || (cache === nextLetter && cacheCounter === 9)) {
       encoded.push(`${cacheCounter}${cache}`);
