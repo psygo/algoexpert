2221464a4b120864970df43366cd3fc1ad4ea32a
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 15:40:24 -0300

    solved the caesar cipher encryptor problem

--- src/easy/caesar_cipher_encryptor.ts
+++ src/easy/caesar_cipher_encryptor.ts
@@ -1,3 +1,20 @@
+type Index = number;
+type Length = number;
+
+// O(n) T | O(1)
+// (ideally, but if there's no efficient way to add to strings, then O(n),
+//  because then we would have to put letters in an array and then rebuild a
+//  string from it)
 export const caesarCipherEncryptor = (string: string, key: number): string => {
-  return "";
+  const length: Length = string.length;
+  let ciphered: string = "";
+
+  for (let i: Index = 0; i < length; i++) {
+    const currentCharCode: number = string.charCodeAt(i);
+    const newCharCode: number = 97 + ((currentCharCode - 97 + key) % 26);
+
+    ciphered += String.fromCharCode(newCharCode);
+  }
+
+  return ciphered;
 };
