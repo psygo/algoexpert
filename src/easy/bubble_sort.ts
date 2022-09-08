c74f45da95dec18230684f2a3b26d89a9e61580d
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 14:11:55 -0300

    minor code improvement to bubble sort

--- src/easy/bubble_sort.ts
+++ src/easy/bubble_sort.ts
@@ -5,15 +5,17 @@ type Length = number;
 export const bubbleSort = (array: number[]): number[] => {
   const length: Length = array.length;
 
-  for (let j: Index = 0; j < length; j++) {
+  for (let j: Index = 0; j < length; j++)
     for (let i: Index = 0; i < length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        const temp: number = array[i + 1];
-        array[i + 1] = array[i];
+      const current: number = array[i];
+      const next: number = array[i + 1];
+
+      if (current > next) {
+        const temp: number = next;
+        array[i + 1] = current;
         array[i] = temp;
       }
     }
-  }
 
   return array;
 };
