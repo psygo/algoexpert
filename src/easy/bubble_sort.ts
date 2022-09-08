8eecd221b6b3f2df9a2053562ca122c8285ded73
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 14:18:04 -0300

    better bubble sort

--- src/easy/bubble_sort.ts
+++ src/easy/bubble_sort.ts
@@ -6,18 +6,25 @@ type Length = number;
 // giving out false if it ever enters the if...
 export const bubbleSort = (array: number[]): number[] => {
   const length: Length = array.length;
+  let isSorted: boolean = false;
 
-  for (let j: Index = 0; j < length; j++)
+  while (!isSorted) {
+    isSorted = true;
+
     for (let i: Index = 0; i < length - 1; i++) {
       const current: number = array[i];
       const next: number = array[i + 1];
 
       if (current > next) {
         const temp: number = next;
+
         array[i + 1] = current;
         array[i] = temp;
+
+        isSorted = false;
       }
     }
+  }
 
   return array;
 };
