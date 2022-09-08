6fb2298b5b7801feb77acd89253d33496d2d3e9f
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-08 20:52:28 -0300

    solved the first non-repeating character problem

--- src/easy/first_non_repeating_character.ts
+++ src/easy/first_non_repeating_character.ts
@@ -1,3 +1,24 @@
+type Index = number;
+type Length = number;
+
+type Letter = string;
+
+const inf: number = 10000000;
+
 export const firstNonRepeatingCharacter = (string: string): number => {
-  return -1;
+  const length: Length = string.length;
+  const repeatMap: Map<Letter, Index[]> = new Map();
+
+  for (let i: Index = 0; i < length; i++) {
+    const currentLetter: Letter = string[i];
+
+    if (!repeatMap.get(currentLetter)) repeatMap.set(currentLetter, [i]);
+    else repeatMap.set(currentLetter, [...repeatMap.get(currentLetter)!, i]);
+  }
+
+  let min: Index = inf;
+  for (const [_, indices] of repeatMap)
+    if (indices.length === 1 && indices[0] < min) min = indices[0];
+
+  return min !== inf ? min : -1;
 };
