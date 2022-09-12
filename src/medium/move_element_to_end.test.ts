63864c863e3c0783e9799faecc371793710ca6d5
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-12 10:43:12 -0300

    solution to the move element to end problem

--- src/medium/move_element_to_end.test.ts
+++ src/medium/move_element_to_end.test.ts
@@ -2,17 +2,17 @@ import * as chai from "chai";
 import { moveElementToEnd } from "./move_element_to_end";
 
 describe("Move Element to End", () => {
-  // it("Test Case #1", function () {
-  //   const array = [2, 1, 2, 2, 2, 3, 4, 2];
-  //   const toMove = 2;
-  //   const expectedStart = [1, 3, 4];
-  //   const expectedEnd = [2, 2, 2, 2, 2];
-  //   const output = moveElementToEnd(array, toMove);
-  //   const outputStart = sorted(output.slice(0, 3));
-  //   const outputEnd = output.slice(3);
-  //   chai.expect(outputStart).to.deep.equal(expectedStart);
-  //   chai.expect(outputEnd).to.deep.equal(expectedEnd);
-  // });
+  it("Test Case #1", function () {
+    const array = [2, 1, 2, 2, 2, 3, 4, 2];
+    const toMove = 2;
+    const expectedStart = [1, 3, 4];
+    const expectedEnd = [2, 2, 2, 2, 2];
+    const output = moveElementToEnd(array, toMove);
+    const outputStart = sorted(output.slice(0, 3));
+    const outputEnd = output.slice(3);
+    chai.expect(outputStart).to.deep.equal(expectedStart);
+    chai.expect(outputEnd).to.deep.equal(expectedEnd);
+  });
 
   it("Test Case #2", function () {
     const array = [1, 2, 3, 4, 5];
@@ -20,8 +20,8 @@ describe("Move Element to End", () => {
     const expectedStart = [1, 2, 4, 5];
     const expectedEnd = [3];
     const output = moveElementToEnd(array, toMove);
-    const outputStart = sorted(output.slice(0, 3));
-    const outputEnd = output.slice(3);
+    const outputStart = sorted(output.slice(0, 4));
+    const outputEnd = output.slice(4);
     chai.expect(outputStart).to.deep.equal(expectedStart);
     chai.expect(outputEnd).to.deep.equal(expectedEnd);
   });
--- src/medium/move_element_to_end.ts
+++ src/medium/move_element_to_end.ts
@@ -4,30 +4,20 @@ type Index = number;
 // O(n) T | O(1) S
 export const moveElementToEnd = (array: number[], toMove: number): number[] => {
   const length: Length = array.length;
+  let i: Index = 0;
+  let j: Index = length - 1;
 
-  let bottomEnd: Index = 0;
-  let lastToMove: Index = length;
-  for (let i: Index = 0; i < length; i++) {
-    const currentNum = array[i];
-    if (currentNum !== toMove && lastToMove < i) {
-      array[bottomEnd] = currentNum;
-      array[i] = toMove;
-      bottomEnd++;
-    } else if (currentNum === toMove) {
-      lastToMove = i;
-      bottomEnd = i;
+  while (i < j) {
+    while (i < j && array[j] === toMove) j--;
+
+    if (array[i] === toMove) {
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
 
-    console.log("--------------------------------------");
-    console.log("Index: ", i);
-    console.log("Current Num: ", currentNum);
-    console.log("Last to Move: ", lastToMove);
-    console.log("Array: ", array);
-    console.log("Bottom End: ", bottomEnd);
-    console.log("--------------------------------------");
+    i++;
   }
-
-  console.log("Final Array: ", array);
 
   return array;
 };
